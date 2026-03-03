852f7f358951cc04cd5ddee59f511fcae08880aa
KJ-Dotzler <kjdotzler@mccneb.edu>
2026-03-03 18:15:14 +0000

    expanded step model to allow for return values, updated front end/JS to show calls and return values

--- backend/api_factorial.py
+++ backend/api_factorial.py
@@ -1,28 +1,52 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List, Dict
 
 router = APIRouter()
 
 class FactorialInput(BaseModel):
-    n : int
+    n : int = Field(gt=0)
 
 class Step(BaseModel):
-    action : str
-    value : str
+    action: str
+    value: str
     depth: int
+    n: int
+    returnVal: int | None = None
 
 class FactorialOutput(BaseModel):
     steps : List[Step]
 
 def factorialSteps(n : int, steps : List[Step], depth: int = 0) -> int:
-    steps.append(Step(action='call', value=f'Factorial({n})', depth=depth))
+    steps.append(
+        Step(
+            action='call', 
+            value=f'Factorial({n})', 
+            depth=depth,
+            n=n,
+            returnVal=None
+            )
+        )
     if n <= 1:
-        steps.append(Step(action='return',value=f'1', depth=depth))
+        steps.append(
+            Step(
+                action='return',
+                value=f'1', 
+                depth=depth,
+                n=n,
+                returnVal=1
+                )
+            )
         return 1
     else:
         result = n * factorialSteps(n-1, steps, depth + 1)
-        steps.append(Step(action='return',value=f'{result}', depth=depth))
+        steps.append(
+            Step(
+                action='return',
+                value=f'{result}', 
+                depth=depth,
+                n=n,
+                returnVal=result))
         return result
 
 @router.post('/factorial', response_model=FactorialOutput)
